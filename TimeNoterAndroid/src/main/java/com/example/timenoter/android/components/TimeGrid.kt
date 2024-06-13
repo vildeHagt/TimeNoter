703556d2f8c103da9d7d5927a9c9eded10b1cac3
Vilde Hagtvedt <vilde.hagtvedt@spv.no>
2024-06-13 13:31:36 +0200

    d

--- TimeNoterAndroid/src/main/java/com/example/timenoter/android/components/TimeGrid.kt
+++ TimeNoterAndroid/src/main/java/com/example/timenoter/android/components/TimeGrid.kt
@@ -1,18 +1,24 @@
 package com.example.timenoter.android.components
 
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.RectangleShape
+import androidx.compose.ui.graphics.Shape
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -30,24 +36,31 @@ fun TimeGrid(timeEntries: List<TimeEntry>) {
         items(timeEntries.size) { index ->
             Column(
                 modifier = Modifier
-                    .padding(8.dp)
-                    .border(2.dp, TimeColors.MellowColors.SoftTeal)
-                    .background(TimeColors.MellowColors.SoftTeal)
+                    .padding(4.dp)
+                    .border(
+                        BorderStroke(1.dp, TimeColors.ModernColors.Blue),
+                        RoundedCornerShape(20)
+                    )
             ) {
-                Text(
-                    text = timeEntries[index].dayStamp,
-                    fontSize = 18.sp,
-                    fontWeight = FontWeight.Bold,
-                    color = Color.Black
-                )
+                Row(
+                    modifier = Modifier.padding(18.dp)
+                        .fillMaxSize(),
+                    horizontalArrangement = Arrangement.SpaceAround
+                ) {
+                    Text(
+                        text = timeEntries[index].dayStamp,
+                        fontSize = 18.sp,
+                        fontWeight = FontWeight.Bold,
+                        color = TimeColors.DarkColors.DarkBlue
+                    )
 
-                Spacer(modifier = Modifier.height(8.dp))
-
-                Text(
-                    text = timeEntries[index].accumulatedTime + " minutter",
-                    fontSize = 16.sp,
-                    color = Color.DarkGray
-                )
+                    Text(
+                        text = timeEntries[index].accumulatedTime + " minutter",
+                        fontSize = 18.sp,
+                        color = TimeColors.DarkColors.DarkBlue,
+                        modifier = Modifier.padding(start = 18.dp)
+                    )
+                }
             }
         }
     }
