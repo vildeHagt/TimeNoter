347ebbf6af73d8a2b3956ff8c158f4b739f7c325
Vilde Hagtvedt <vilde.hagtvedt@spv.no>
2024-11-07 13:36:51 +0100

    implementere firebase

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,4 +4,5 @@ plugins {
     alias(libs.plugins.androidLibrary).apply(false)
     alias(libs.plugins.kotlinAndroid).apply(false)
     alias(libs.plugins.kotlinMultiplatform).apply(false)
+    id("com.google.gms.google-services") version "4.4.2" apply false
 }
